2bcfbcd266b0fe00dd79a52d436b73f5fa8dc570
Guilherme Ribeiro <gribeiro.informatica@gmail.com>
2024-02-23 00:48:55 -0300

    Ajuste SideBar

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import jQurery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
-}
+
+  ngAfterViewInit(){
+    window.addEventListener('DOMContentLoaded', event => {
+
+      // Toggle the side navigation
+      const sidebarToggle = document.body.querySelector('#sidebarToggle');
+      if (sidebarToggle) {
+          // Uncomment Below to persist sidebar toggle between refreshes
+          // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+          //     document.body.classList.toggle('sb-sidenav-toggled');
+          // }
+          sidebarToggle.addEventListener('click', event => {
+              event.preventDefault();
+              document.body.classList.toggle('sb-sidenav-toggled');
+              localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+          });
+      }
+  
+  });
+  }
+      
+  }
+

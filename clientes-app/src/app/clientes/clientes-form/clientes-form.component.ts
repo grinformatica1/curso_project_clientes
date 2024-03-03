1840214346d5d7841ac630d8a966381913a9b7f8
Guilherme Ribeiro <gribeiro.informatica@gmail.com>
2024-03-03 11:50:05 -0300

    Conexão com o Back e envio de informação ao BD

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../cliente'
+import { ClientesService } from '../../clientes.service'
 
 @Component({
   selector: 'app-clientes-form',
@@ -11,9 +12,17 @@ export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
 
-  constructor() { }
+  constructor( private service: ClientesService ) {
+    this.cliente = new Cliente();
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.service.salvar(this.cliente).subscribe( response => {
+      console.log(response);
+    } )
   }
 
 }

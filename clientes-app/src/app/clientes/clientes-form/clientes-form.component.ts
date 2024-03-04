88efe335032fcd3627c2b82c742f4a383eda2a46
Guilherme Ribeiro <gribeiro.informatica@gmail.com>
2024-03-03 23:00:33 -0300

    Ajuste de tela de inclusão de clientes

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
 
 import { Cliente } from '../cliente'
 import { ClientesService } from '../../clientes.service'
@@ -11,17 +13,31 @@ import { ClientesService } from '../../clientes.service'
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
 
-  constructor( private service: ClientesService ) {
+  constructor( 
+    private service: ClientesService ,
+    private router: Router
+    ) {
     this.cliente = new Cliente();
    }
 
   ngOnInit(): void {
   }
 
+  voltarParaListagem(){
+    this.router.navigate(['/clientes-lista'])
+  }
+
   onSubmit(){
     this.service.salvar(this.cliente).subscribe( response => {
-      console.log(response);
+      this.success = true;
+      this.errors = null;
+      this.cliente = response;
+    }, errorResponse => {
+      this.success = false;
+      this.errors = errorResponse.error.errors;
     } )
   }
 
--- clientes/src/main/java/io/github/grinformatica1/clientes/rest/ClienteController.java
+++ clientes/src/main/java/io/github/grinformatica1/clientes/rest/ClienteController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/clientes")
 @CrossOrigin("http://localhost:4200")
@@ -18,6 +20,11 @@ public class ClienteController {
     @Autowired
     public ClienteController(ClienteRepository repository){
         this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Cliente> obterTodos(){
+        return repository.findAll();
     }
 
     @PostMapping

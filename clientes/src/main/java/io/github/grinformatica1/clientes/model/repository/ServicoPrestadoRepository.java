f48ec9fdc5bccc966b76d9461b4e884c293e5448
Guilherme Ribeiro <gribeiro.informatica@gmail.com>
2024-03-07 01:01:28 -0300

    Criando APÌ de serviço prestado

---
+++ clientes/src/main/java/io/github/grinformatica1/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,8 @@
+package io.github.grinformatica1.clientes.model.repository;
+
+import io.github.grinformatica1.clientes.model.entity.ServicoPrestado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+}
--- clientes/src/main/java/io/github/grinformatica1/clientes/model/repository/ServicoRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.grinformatica1.clientes.model.repository;
-
-import io.github.grinformatica1.clientes.model.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-
-}
